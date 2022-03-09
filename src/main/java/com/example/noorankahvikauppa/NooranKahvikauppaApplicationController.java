8033cac737cbacc9cd2b664823a2e96f0ce285fb
JiiKoo21 <90307164+JiiKoo21@users.noreply.github.com>
2022-03-09 12:59:13 +0200

    Muutetaan controllerissa ohjausta etusivulle

--- src/main/java/com/example/noorankahvikauppa/NooranKahvikauppaApplicationController.java
+++ src/main/java/com/example/noorankahvikauppa/NooranKahvikauppaApplicationController.java
@@ -2,14 +2,12 @@ package com.example.noorankahvikauppa;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class NooranKahvikauppaApplicationController {
   
     @GetMapping("*")
-    @ResponseBody
     public String home() {
-      return "Morjensta!";
+      return "index";
   }  
 }
