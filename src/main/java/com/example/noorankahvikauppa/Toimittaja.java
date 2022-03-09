9d96c3361deb67f669e1054da8e69c7b4eeff9b6
JiiKoo21 <90307164+JiiKoo21@users.noreply.github.com>
2022-03-09 15:01:26 +0200

    Muokataan Toimittajaa

--- src/main/java/com/example/noorankahvikauppa/Toimittaja.java
+++ src/main/java/com/example/noorankahvikauppa/Toimittaja.java
@@ -1,5 +1,29 @@
 package com.example.noorankahvikauppa;
 
-public class Toimittaja {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Toimittaja extends AbstractPersistable<Long> {
+
+    private String nimi;
+    private String yhteyshenkilo;
+    private String yhteyshenkiloemail;
+
+    // YHDELLÄ TOIMITTAJALLA VOI OLLA YKSI TAI USEAMPI TUOTE
+    @OneToMany(mappedBy = "toimittaja")
+    private List<Tuote> tuotteet = new ArrayList<>();
+
 }
