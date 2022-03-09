0584ae7b55afd6ddd8fb7f1867a733cc43769427
JiiKoo21 <90307164+JiiKoo21@users.noreply.github.com>
2022-03-09 12:21:44 +0200

    Perustetaan uusi projekti ohjeistuksen mukaisesti

---
+++ src/main/java/com/example/noorankahvikauppa/NooranKahvikauppaApplication.java
@@ -0,0 +1,13 @@
+package com.example.noorankahvikauppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NooranKahvikauppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NooranKahvikauppaApplication.class, args);
+	}
+
+}
