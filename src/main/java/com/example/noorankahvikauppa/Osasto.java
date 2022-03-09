af18905288a4dc31db2f48c3ca00c8664388df05
JiiKoo21 <90307164+JiiKoo21@users.noreply.github.com>
2022-03-09 14:57:27 +0200

    Muokataan Osastoa

--- src/main/java/com/example/noorankahvikauppa/Osasto.java
+++ src/main/java/com/example/noorankahvikauppa/Osasto.java
@@ -1,5 +1,26 @@
 package com.example.noorankahvikauppa;
 
-public class Osasto {
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Osasto extends AbstractPersistable<Long> {
+
+    private String nimi;
+    private Long osastoIDP;
+
+    // YHDESSÄ OSASTOSSA VOI OLLA YKSI TAI USEAMPI TUOTE
+    @OneToMany(mappedBy = "osasto")
+    private List<Tuote> tuotteet = new ArrayList<>();
     
 }
