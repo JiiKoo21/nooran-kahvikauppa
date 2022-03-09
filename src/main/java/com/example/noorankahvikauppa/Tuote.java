54d7ab2aef2a69859357a8e3e50208535a3d3edf
JiiKoo21 <90307164+JiiKoo21@users.noreply.github.com>
2022-03-09 15:13:50 +0200

    Muokataan tuote (huom: kuva!)

--- src/main/java/com/example/noorankahvikauppa/Tuote.java
+++ src/main/java/com/example/noorankahvikauppa/Tuote.java
@@ -1,5 +1,44 @@
 package com.example.noorankahvikauppa;
 
-public class Tuote {
+import java.math.BigDecimal;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.Type;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tuote extends AbstractPersistable<Long> {
+
+    private String nimi;
+    private String kuvaus;
+    private BigDecimal hinta;
     
+    // MUISTA STRING KUVA
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    @Basic(fetch = FetchType.EAGER)
+    private byte[] kuva;
+    
+    // TUOTE LIITTYY YHTEEN OSASTOON, VALMISTAJAAN JA TOIMITTAJAAN
+    @ManyToOne
+    private Osasto osasto;
+    
+    @ManyToOne
+    private Valmistaja valmistaja;
+    
+    @ManyToOne
+    private Toimittaja toimittaja;
+
 }
