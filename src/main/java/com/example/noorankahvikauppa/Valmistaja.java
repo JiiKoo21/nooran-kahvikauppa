1ef371f8d8fabd824388160f6aac767457ade3bd
JiiKoo21 <90307164+JiiKoo21@users.noreply.github.com>
2022-03-09 15:06:34 +0200

    Muokataan Valmistajaa

--- src/main/java/com/example/noorankahvikauppa/Valmistaja.java
+++ src/main/java/com/example/noorankahvikauppa/Valmistaja.java
@@ -1,5 +1,27 @@
 package com.example.noorankahvikauppa;
 
-public class Valmistaja {
-    
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Valmistaja extends AbstractPersistable<Long> {
+
+    private String nimi;
+    private String url;
+
+    // YHDELLÄ VALMISTAJALLA VOI OLLA YKSI TAI USEAMPI TUOTE
+    @OneToMany(mappedBy = "valmistaja")
+    private List<Tuote> tuotteet = new ArrayList<>();
+
 }
